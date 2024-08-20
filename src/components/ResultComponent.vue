5757c1248795fe1689fc03559b72249d15829e3d
Alexandra Balo <alexandrabalo@yahoo.com>
2024-08-20 20:30:00 +0300

    added start page

--- src/components/ResultComponent.vue
+++ src/components/ResultComponent.vue
@@ -1,53 +1,46 @@
 <template>
     <div class="result-container">
-        <video width="500" autoplay loop>
-            <source src="../assets/result.mp4" type="video/mp4">
-            Your browser does not support the video tag.
-        </video>
-        <p class="result">{{ score }} / {{ numberOfQuestions }}</p>
+        <img src="../assets/result.png" class="result-image" alt="result image">
+        <p class="result">{{score}} / {{numberOfQuestions}}</p>
         <button @click="retry">Retry</button>
     </div>
 </template>
 
 <script>
-export default {
-    name: 'ResultComponent',
-    props: ['numberOfQuestions', 'score'],
-    emits: ['retry'],
-    methods: {
-        retry() {
-            this.$emit('retry');
+    export default {
+        name: 'ResultComponent',
+        props: ['numberOfQuestions','score'],
+        emits: ['retry'],
+        methods: {
+            retry(){
+                this.$emit('retry');
+            }
         }
     }
-}
 </script>
 
 <style scoped>
-.result-container {
+.result-container{
     display: flex;
     flex-direction: column;
     justify-content: center;
     z-index: 1;
 }
 
-.result {
+.result{
     font-size: xxx-large;
     text-align: center;
     padding-bottom: 50px;
 }
 
-.result-image {
+.result-image{
     height: 400px;
     width: 400px;
     margin: auto;
 }
 
-button {
+button{
     width: 100px;
-    margin: 0 auto;
-}
-
-video{
     margin: 0 auto;
 }
 </style>
